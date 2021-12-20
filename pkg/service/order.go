d7237146a53f66584efaafb9a6bfedafac4efa75
duchieu <duchieu.ctk41@gmail.com>
2021-12-20 10:47:16 +0700

    hieucn/fix/send-email-order

--- pkg/service/order.go
+++ pkg/service/order.go
@@ -856,7 +856,7 @@ func (s *OrderService) SendEmailOrder(ctx context.Context, req model.SendEmailRe
 	var params interface{} = tParams
 	body := sendinblue.SendSmtpEmail{
 		Sender: &sendinblue.SendSmtpEmailSender{
-			Name:  businessInfo.Name,
+			Name:  businessInfo.Name, //
 			Email: utils.DefaultFromEmail,
 		},
 		To:     []sendinblue.SendSmtpEmailTo{to},
