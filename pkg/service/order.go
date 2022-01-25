21b07b55195c60c4ddd723fc7f3dbf4841223aee
hieuhoccode <duchieu.ctk41@gmail.com>
2022-01-25 12:58:24 +0700

    hieucn/fix/free-ship-for-order-suitable

--- pkg/service/order.go
+++ pkg/service/order.go
@@ -236,6 +236,7 @@ func (s *OrderService) CreateOrder(ctx context.Context, req model.OrderBody) (re
 		}
 		break
 	case utils.SELLER_CREATE_METHOD:
+
 		tUser, err := s.GetUserList(ctx, req.BuyerInfo.PhoneNumber, "")
 		if err != nil {
 			log.WithError(err).Error("Error when get user info from phone number of buyer info")
@@ -249,6 +250,12 @@ func (s *OrderService) CreateOrder(ctx context.Context, req model.OrderBody) (re
 	default:
 		log.WithError(err).Error("Error when Create method, expected: [buyer, seller]")
 		return nil, ginext.NewError(http.StatusBadRequest, utils.MessageError()[http.StatusBadRequest])
+	}
+
+	if info.Data.Business.DeliveryFee == 0 || (info.Data.Business.DeliveryFee > 0 && orderGrandTotal >= info.Data.Business.MinPriceFreeShip && info.Data.Business.MinPriceFreeShip > 0) {
+		deliveryFee = 0
+	} else {
+		deliveryFee = info.Data.Business.DeliveryFee
 	}
 
 	if req.DeliveryMethod != nil && *req.DeliveryMethod == utils.DELIVERY_METHOD_BUYER_PICK_UP {
@@ -2018,12 +2025,6 @@ func (s *OrderService) CreateOrderV2(ctx context.Context, req model.OrderBody) (
 		// with buyer state always waiting confirm
 		req.State = utils.ORDER_STATE_WAITING_CONFIRM
 
-		buyerID = req.UserID
-		if info.Data.Business.DeliveryFee == 0 || (info.Data.Business.DeliveryFee > 0 && orderGrandTotal >= info.Data.Business.MinPriceFreeShip && info.Data.Business.MinPriceFreeShip > 0) {
-			deliveryFee = 0
-		} else {
-			deliveryFee = info.Data.Business.DeliveryFee
-		}
 		break
 	case utils.SELLER_CREATE_METHOD:
 		// check buyer received or not
@@ -2149,6 +2150,16 @@ func (s *OrderService) CreateOrderV2(ctx context.Context, req model.OrderBody) (
 		}
 		req.ListOrderItem[i].TotalAmount = math.Round(itemTotalAmount)
 		orderGrandTotal += req.ListOrderItem[i].TotalAmount
+	}
+
+	// check if order is match condition free ship
+	if req.CreateMethod == utils.BUYER_CREATE_METHOD {
+		//buyerID = &req.Body.UserId
+		if info.Data.Business.DeliveryFee == 0 || (info.Data.Business.DeliveryFee > 0 && orderGrandTotal >= info.Data.Business.MinPriceFreeShip && info.Data.Business.MinPriceFreeShip > 0) {
+			deliveryFee = 0
+		} else {
+			deliveryFee = info.Data.Business.DeliveryFee
+		}
 	}
 
 	if req.DeliveryMethod != nil && *req.DeliveryMethod == utils.DELIVERY_METHOD_BUYER_PICK_UP {
